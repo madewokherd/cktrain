1734e03a86f4957159ba67cd500ea7ce1edec69d
Vincent Povirk <madewokherd@gmail.com>
2016-10-30 12:45:53 -0500

    Some RNG and architecture.

--- main.c
+++ main.c
@@ -1,6 +1,138 @@
 #include <windows.h>
 #include <stdio.h>
 
+typedef struct obstacle {
+	int index;
+	const wchar_t *name;
+} obstacle;
+
+const struct obstacle obstacles[] = {
+	{0, L"Fireballs"},
+	//{1, L"Fire Rings"},
+	{3, L"Spikes"},
+	{4, L"Falling Blocks"},
+	{5, L"Flying Blobs"},
+	{6, L"Firespinners"},
+	{7, L"Moving Blocks"},
+	{8, L"Elevators"},
+	{9, L"Boulders"},
+	{10, L"Spikey Guys"},
+	{11, L"Spikey Line"},
+	{12, L"Lasers"},
+	{13, L"Ghost Blocks"},
+	{14, L"Bouncy BLocks"},
+	{15, L"Clouds"},
+	//{16, L"Conveyors"},
+	{17, L"Pendulums"},
+	{18, L"Serpent"},
+	{19, L"Sludge"},
+	{21, L"Level Speed"},
+	{22, L"Jump Difficulty"},
+	{23, L"Ceilings"},
+};
+
+#define NUM_OBSTACLES 24
+#define MAX_OBSTACLE_SETTING 32
+
+typedef unsigned char permutation[NUM_OBSTACLES];
+
+typedef struct permutation_info {
+	permutation permutation;
+	int difficulty_numerator;
+	int difficulty_denominator;
+} permutation_info;
+
+typedef struct permutation_infos {
+	permutation_info *items;
+	int len;
+	int size;
+} permutation_infos;
+
+permutation_infos global_infos;
+
+static void append_info(permutation_infos *list, const permutation_info *item)
+{
+	if (list->len == list->size)
+	{
+		if (list->size)
+			list->size = list->size * 2;
+		else
+			list->size = 512;
+		list->items = realloc(list->items, list->size * sizeof(*list->items));
+	}
+	list->items[list->len++] = *item;
+}
+
+static void free_infos(permutation_infos *list)
+{
+	free(list->items);
+	list->items = NULL;
+	list->len = list->size = 0;
+}
+
+static BOOL is_superset(const permutation* sub, const permutation* sup)
+{
+	int i;
+
+	for (i = 0; i < NUM_OBSTACLES; i++)
+	{
+		if (sub[i] > sup[i])
+			return FALSE;
+	}
+
+	return TRUE;
+}
+
+static void get_random_bits(char* output, int output_len)
+{
+	static HCRYPTPROV hprov;
+
+	if (!hprov)
+	{
+		CryptAcquireContextW(&hprov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT);
+	}
+
+	CryptGenRandom(hprov, output_len, output);
+}
+
+static int get_random_bit()
+{
+	static char bits;
+	static char next_bit = 0;
+	int result;
+
+	if (!next_bit)
+	{
+		get_random_bits(&bits, 1);
+		next_bit = 1;
+	}
+
+	result = (bits & next_bit) != 0;
+
+	next_bit = (next_bit << 1) & 0xff;
+
+	return result;
+}
+
+static int randint(int lower, int upper)
+{
+	int range = upper - lower;
+	int gen_upper;
+	int result;
+
+	do {
+		gen_upper = 1;
+		result = 0;
+		while (gen_upper <= range)
+		{
+			gen_upper <<= 1;
+			result = (result << 1) | get_random_bit();
+		}
+	} while (result > range);
+
+	return result + lower;
+}
+
 static wchar_t* strdup_wprintf(const wchar_t *format, ...)
 {
 	va_list args;
@@ -39,7 +171,7 @@ static LRESULT CALLBACK main_wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM 
 
 		save = SaveDC(hdc);
 
-		output = strdup_wprintf(L"Level code:\n%s\n\nDifficulty: %.1f%%", L"LEVELCODE", 99.9);
+		output = strdup_wprintf(L"Level code:\n%s\n\nDifficulty: %.1f%%", L"LEVELCODE", randint(0,999) / 10.0);
 
 		GetClientRect(hwnd, &client);
 
